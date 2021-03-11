d86583cde0444fc35e2cd254120a65d939729f5e
Alvin Prenda Laroya <53330353+wazaskiller@users.noreply.github.com>
2021-03-11 14:13:02 +0800

    .

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-/* import categories from './modules/categories';
-import restaurants from './modules/restaurants';
-import menus from './modules/menus'; */
 import organizations from './modules/organizations';
 import persons from './modules/persons';
 
